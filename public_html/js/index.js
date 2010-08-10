ed46af725198446b04af4f6775da9f4f98958711
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-08-11 01:54:10 +0400

    something static

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,9 +1,33 @@
 var lastStamp = 0;
+var jobTemplate;
+var jobPlace;
 
-function init() {
-	$('ul.job').fadeIn('slow');
+function addJob(job) {
+	jobEl = jobTemplate.clone();
+	
+//	alert(job.description);
+
+	$('li.title', jobEl).html(job.title);
+	$('li.desc', jobEl).html(job.description);
+	$('li.time', jobEl).html(job.time);
+	
+	jobEl.appendTo(jobPlace).fadeIn('slow');
 }
 
-$( function() {
-	init();
-} );
+function init() {
+	jobTemplate = $('ul.job:first');
+	jobPlace    = $('#right');
+	
+	// removing right content
+	$('#right > *').remove();
+	
+	job = {
+		title: 'test',
+		description: 'desc',
+		time: '23:20'
+	};
+	
+	addJob(job);
+	
+//	jobTemplate.appendTo(jobPlace).show();
+}

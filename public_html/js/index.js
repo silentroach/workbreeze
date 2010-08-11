7325d2bc70ac3e09eb8ab52759c6e89421472c2f
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-08-11 23:09:51 +0400

    closure + addJob js method

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,22 +1,56 @@
+/**
+ * @type {number}
+ */
 var lastStamp = 0;
+var checkTimer;
+/** @const */ var checkInterval = 30000;
 var jobTemplate;
 var jobPlace;
+var queue = [];
+
+function dropTimer() {
+	if (null != checkTimer) {
+		clearTimeout(checkTimer);
+	}
+}
+
+function setTimer(interval) {
+	dropTimer();
+	checkTimer = setInterval(function() { checkQueue(); }, 5000);
+}
+
+function popFromQueue() {
+	tmpEl = queue.pop();
+	
+	tmpEl
+		.prependTo(jobPlace)
+		.slideDown('slow');
+}
+
+function checkQueue() {
+	if (queue.length > 0)
+		popFromQueue();
+}
 
 function addJob(job) {
 	jobEl = jobTemplate.clone();
 	
 //	alert(job.description);
 
+	jobEl.hide();
+
 	$('li.title', jobEl).html(job.title);
 	$('li.desc', jobEl).html(job.description);
 	$('li.time', jobEl).html(job.time);
 	
-	jobEl.appendTo(jobPlace).fadeIn('slow');
+	queue.push(jobEl);
 }
 
 function init() {
 	jobTemplate = $('ul.job:first');
 	jobPlace    = $('#right');
+	
+	setTimer(5000);
 	
 	// removing right content
 	$('#right > *').remove();
@@ -27,7 +61,18 @@ function init() {
 		time: '23:20'
 	};
 	
+	job2 = {
+		title: 'blabla',
+		description: 'hrenhren',
+		time: '21:09'
+	}
+	
 	addJob(job);
+	addJob(job2);
 	
 //	jobTemplate.appendTo(jobPlace).show();
 }
+
+$( function() {
+	init();
+} );

482499e2a56c7140dc36733c939113a2ff3b4b7d
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-08-13 00:19:23 +0400

    new php ajax part

--- ajax/index.php
+++ ajax/index.php
@@ -1,3 +1,36 @@
 <?php
 
-echo 'test';
+function fail() {
+	header('HTTP/1.0 404 Not Found');
+	die();
+}
+
+if (!isset($_SERVER['REQUEST_URI']))
+	fail();
+	
+$uri = $_SERVER['REQUEST_URI'];
+
+$query = explode('/', $uri);
+
+array_shift($query);
+
+$module = array_shift($query);
+
+if (null === $module)
+	fail();
+	
+$here = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+
+require($here . 'module.php');
+
+$mname = $here . 'modules' . DIRECTORY_SEPARATOR . $module . '.php';
+
+if (!file_exists($mname))
+	fail();
+	
+$className = 'M' . ucfirst($module);
+	
+require($mname);
+
+$module = new $className($query);
+$module->run();
---
+++ ajax/module.php
@@ -0,0 +1,15 @@
+<?php
+
+class Module {
+
+	private $request;
+
+	public function __construct($request) {
+		$this->request = $request;
+	}
+
+	public function run() {
+
+	}
+
+}

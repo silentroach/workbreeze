40a485586439826afc8e3a67c7b80b3689b86302
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-08-21 14:02:48 +0400

    hack use simple defence

--- index.php
+++ index.php
@@ -8,6 +8,21 @@ function fail() {
 if (!isset($_SERVER['REQUEST_URI'])) {
 	fail();
 }
+
+require('defines.php');
+
+if (
+	!defined('DEBUG')
+	&& (
+		!isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+		|| !isset($_SERVER['HTTP_REFERER'])
+		|| !isset($_SERVER['HTTP_HOST'])
+		|| 'XMLHttpRequest' !== $_SERVER['HTTP_X_REQUESTED_WITH']
+		|| false !== strpos($_SERVER['HTTP_HOST'], $_SERVER['HTTP_REFERER'])
+	)
+) {
+	fail();
+}
 	
 $uri = $_SERVER['REQUEST_URI'];
 
@@ -23,7 +38,6 @@ if (null === $module) {
 	fail();
 }
 
-require('defines.php');	
 require(PATH_CLASSES . 'module.php');
 
 $mname = PATH_CLASSES . 'modules' . DIRECTORY_SEPARATOR . $module . '.php';

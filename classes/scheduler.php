27c9aba3b6ddafe4f7fc6343e59f91437ec8d9e0
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-09-07 00:03:01 +0400

    global rss generator

--- classes/scheduler.php
+++ classes/scheduler.php
@@ -20,6 +20,65 @@ class Scheduler {
 		return $this->parsers[$info['code']];
 	}
 
+	public function updateGlobalRSS() {
+		echo "Updating global RSS channel...\n";
+		
+		$sites = $this->db->sites;
+		
+		$st = array();
+		
+		$cursor = $this->db->jobs->find();
+		$cursor->sort(array('stamp' => -1));
+		$cursor->limit(25);
+		
+		date_default_timezone_set('GMT');
+		
+		$writer = new XMLWriter();
+		$writer->openURI(PATH_PUBLIC . 'jobs/rss-global.xml');
+		if (defined('DEBUG')) {
+			$writer->setIndent(4);
+		}
+		$writer->startDocument('1.0');
+		$writer->startElement('rss');
+		$writer->writeAttribute('version', '2.0');
+		$writer->writeAttribute('xmlns:atom', 'http://www.w3.org/2005/Atom');		
+		
+		$writer->startElement('channel');
+		$writer->writeElement('title', 'WorkBreeze');
+		$writer->writeElement('description', 'WorkBreeze');
+		$writer->writeElement('link', 'http://workbreeze.com');
+		$writer->writeElement('pubDate', date('D, d M Y H:i:s e'));
+		
+		while ($item = $cursor->getNext()) {
+			$writer->startElement('item');
+			
+			if (!isset($st[$item['site']])) {
+				$st[$item['site']] = $sites->findOne(array('code' => $item['site']));
+			}
+			
+			$s = $st[$item['site']];
+			
+			$writer->writeElement('title', $s['name'] . ': ' . $item['title']);
+			$writer->writeElement('link', $item['url']);
+			$writer->startElement('description');
+			$writer->writeCData($item['desc']);
+			$writer->endElement();
+			
+			$writer->writeElement('guid', 'http://workbreeze.com/jobs/' . 
+				$s['folder'] . '/' . $item['id'] . '.html');
+				
+			$writer->writeElement('pubDate', date('D, d M Y H:i:s e', $item['stamp']));
+			
+			$writer->endElement();
+		}
+		
+		$writer->endElement(); // </channel>
+		$writer->endElement(); // </rss>
+		$writer->endDocument();
+		
+		$writer->flush();
+	}
+
 	public function processJobList() {
 		$c = $this->db->sites->find();
 		
@@ -44,6 +103,8 @@ class Scheduler {
 					'count' => $parser->getQueuedCount()
 				));
 			}
+
+			$this->updateGlobalRSS();
 		}
 	}
 	

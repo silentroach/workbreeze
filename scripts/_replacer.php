716396883fec31f2d49f16482869fd399601b905
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-09-17 00:20:57 +0400

    icons are all in css

---
+++ scripts/_replacer.php
@@ -0,0 +1,26 @@
+<?php
+
+function replaceTag($content, $tag, $replacement) {
+        $l = mb_strlen($tag, 'UTF-8') + 6;
+
+        $i = mb_strpos($content, '/* <' . $tag . '> */', 0, 'UTF-8');
+        if (false === $i) {
+                break;
+        }
+
+        $i += $l + 2;
+
+        $n = mb_strpos($content, '/* </' . $tag . '> */', 0, 'UTF-8');
+        if (false === $n) {
+                echo 'Final tag not found\n';
+                die();
+        }
+
+        $n -= $l;
+
+        $replacement = "\n" . $replacement . "\n";
+
+        $content = mb_substr($content, 0, $i) . $replacement . mb_substr($content, $n + $l, mb_strlen($content) - $n - $l);
+
+        return $content;
+}

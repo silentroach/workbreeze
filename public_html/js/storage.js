edae315aa8045595204c9ec01f2944d2b70cdde2
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-11-03 17:22:28 +0300

    trying to complete google closure lint without errors in strict mode

--- public_html/js/storage.js
+++ public_html/js/storage.js
@@ -1,8 +1,8 @@
 /* <debug> */
 if ('localStorage' in window) {
-	console.info('local storage enabled');
+  console.info('local storage enabled');
 } else {
-	console.warn('local storage not available');
+  console.warn('local storage not available');
 }
 /* </debug> */
 
@@ -12,70 +12,68 @@ if ('localStorage' in window) {
  * @this {workbreeze.storage}
  */
 workbreeze.storage = function() {
-	var self = this;
-	
-	/**
-	 * Cached items
-	 * @type {Array}
-	 */
-	var cache = [];
+  var self = this;
 
-	/**
-	 * Is localStorage enabled in this browser?
-	 * @type {Boolean}
-	 * @private
-	 */
-	var enabled = ('localStorage' in window);
-	
-	/**
-	 * Get object from storage
-	 * @param {!string} itemName Storage item name
-	 * @return {(Object|boolean)}
-	 */
-	self.get = function(itemName) {
-		if (!enabled)
-			return false;
-			
-		if (!(itemName in cache)) {
-			cache[itemName] = $.parseJSON(localStorage.getItem(itemName)) || false;
-		}
-			
-		return cache[itemName];
-	};
-	
-	/**
-	 * Store object
-	 * @param {!string} itemName Storage item name
-	 * @param {(Object|Array)} object Object to store
-	 * @param {?number} version Version of object
-	 */
-	self.set = function(itemName, object, version) {
-		if (!enabled)
-			return;
+  /**
+   * Cached items
+   * @type {Array}
+   */
+  var cache = [];
 
-		var str = JSON.stringify((undefined === version ? object : [version, object]));
+  /**
+   * Is localStorage enabled in this browser?
+   * @type {Boolean}
+   * @private
+   */
+  var enabled = ('localStorage' in window);
 
-		localStorage.setItem(itemName, str);	
-	};
+  /**
+   * Get object from storage
+   * @param {!string} itemName Storage item name.
+   * @return {(Object|boolean)} Storage item value.
+   */
+  self.get = function(itemName) {
+    if (!enabled)
+      return false;
 
-	/**
-	 * Get version of storage item
-	 * @param {!string} itemName Storage item name
-	 * @return {number}
-	 */
-	self.getVersion = function(itemName) {
-		if (!enabled)
-			return 0;
-		
-		var item = self.get(itemName);
+    if (!(itemName in cache)) {
+      cache[itemName] = $.parseJSON(localStorage.getItem(itemName)) || false;
+    }
 
-		if (
-			!item
-			|| item.length != 2
-		) {
-			return 0;
-		}
+    return cache[itemName];
+  };
 
-		return Math.floor(item[0]);
-	};	
+  /**
+   * Store object
+   * @param {!string} itemName Storage item name.
+   * @param {(Object|Array)} object Object to store.
+   * @param {?number} version Version of object.
+   */
+  self.set = function(itemName, object, version) {
+    if (!enabled)
+      return;
+
+    var str = JSON.stringify((undefined === version ?
+                              object : [version, object]));
+
+    localStorage.setItem(itemName, str);
+  };
+
+  /**
+   * Get version of storage item
+   * @param {!string} itemName Storage item name.
+   * @return {number} Storage item version.
+   */
+  self.getVersion = function(itemName) {
+    if (!enabled)
+      return 0;
+
+    var item = self.get(itemName);
+
+    if (!item || item.length != 2) {
+      return 0;
+    }
+
+    return Math.floor(item[0]);
+  };
 };

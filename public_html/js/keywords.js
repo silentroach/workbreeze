008a252a0c68a3b62bde7059b452023fb60aa62b
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-11-03 18:00:02 +0300

    google closure lint is really hard >_<

--- public_html/js/keywords.js
+++ public_html/js/keywords.js
@@ -2,113 +2,109 @@
  * Keywords filter object
  * @constructor
  * @this {workbreeze.keywords}
- * @param {Object} s Options
+ * @param {Object} s Options.
  */
 workbreeze.keywords = function(s) {
-	var self = this;
-	
-	/**
-	 * Options
-	 * @type {Object}
-	 */
-	var options = $.extend( {
-		place: '#keywords',
-		handlePause: 2000
-	}, s);
-	
-	var keywordTimer = null;
-	var place = $(options.place);
-	
-	/**
-	 * Array of used keywords
-	 * @type {Array} 
-	 */
-	var keys = [];
-	
-	/**
-	 * Filter item identifier
-	 * @const
-	 * @type {string}
-	 */
-	self.identifier = 'keys';
+  var self = this;
 
-	/**
-	 * Filter item value
-	 * @return {Array}
-	 */
-	self.getValue = function() {
-		return keys;
-	}
-	
-	/**
-	 * Set the filter item value
-	 * @param {Object|string} value
-	 */
-	self.setValue = function(value) {
-		keys = value || [];
-		
-		place.val(keys.join(', '));
-	}
+  /**
+   * Options
+   * @type {Object}
+   */
+  var options = $.extend({
+    place: '#keywords',
+    handlePause: 2000
+  }, s);
 
-	/**
-	 * Check the job element
-	 * @param {jQuery} jobElement Job Element
-	 * @return {boolean}
-	 */
-	self.checkJob = function(jobElement) {
-		if (0 === keys.length) {
-			return true;
-		}
+  var keywordTimer = null;
+  var place = $(options.place);
 
-		var str = $('li.k', jobElement).html() || '';		
+  /**
+   * Array of used keywords
+   * @type {Array}
+   */
+  var keys = [];
 
-		for (var i = 0; i < keys.length; i++) {
-			if (str.indexOf(keys[i].toLowerCase()) >= 0) {
-				return true;
-			}
-		}
+  /**
+   * Filter item identifier
+   * @const
+   * @type {string}
+   */
+  self.identifier = 'keys';
 
-		return false;
-	}
+  /**
+   * Filter item value
+   * @return {Array} Item value.
+   */
+  self.getValue = function() {
+    return keys;
+  }
 
-	/**
-	 * onChanged handler
-	 */
-	self.onChanged = function() { };
-	
-	var changed = function() {
-		keys = [];
-	
-		var tmp = place.val().split(',');
+  /**
+   * Set the filter item value
+   * @param {Object|string} value Item value.
+   */
+  self.setValue = function(value) {
+    keys = value || [];
 
-		$(tmp).each( function() {
-			var key = $.trim(this);
-			
-			if (
-				'' !== key
-				&& $.inArray(key, keys) < 0
-			) {
-				keys.push(key);
-			}
-		} );
-	
-		self.onChanged();
-	}
-	
-// ------------------------------------------------------
-// initialization
-// ------------------------------------------------------
+    place.val(keys.join(', '));
+  }
 
-	place
-		.keyup(function(e) {
-			if (null != keywordTimer) {
-				clearTimeout(keywordTimer);
-			}
-	
-			if (e.keyCode == 13) {
-				changed();
-			} else {
-				keywordTimer = setTimeout(changed, options.handlePause);
-			}
-		});
-}
+  /**
+   * Check the job element
+   * @param {jQuery} jobElement Job Element.
+   * @return {boolean} Job is correct.
+   */
+  self.checkJob = function(jobElement) {
+    if (0 === keys.length) {
+      return true;
+    }
+
+    var str = $('li.k', jobElement).html() || '';
+
+    for (var i = 0; i < keys.length; i++) {
+      if (str.indexOf(keys[i].toLowerCase()) >= 0) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * onChanged handler
+   */
+  self.onChanged = function() { };
+
+  var changed = function() {
+    keys = [];
+
+    var tmp = place.val().split(',');
+
+    $(tmp).each(function() {
+      var key = $.trim(this);
+
+      if ('' !== key && $.inArray(key, keys) < 0) {
+        keys.push(key);
+      }
+    });
+
+    self.onChanged();
+  }
+
+  // ------------------------------------------------------
+  // initialization
+  // ------------------------------------------------------
+
+  place.keyup(function(e) {
+    if (null != keywordTimer) {
+      clearTimeout(keywordTimer);
+    }
+
+    if (e.keyCode == 13) {
+      changed();
+    } else {
+      keywordTimer = setTimeout(changed, options.handlePause);
+    }
+  });
+};

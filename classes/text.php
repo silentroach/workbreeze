e94df34ccd62db21e9ba74319590080b0e791c50
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-11-17 22:58:27 +0300

    some text beautifier improvements

--- classes/text.php
+++ classes/text.php
@@ -3,13 +3,85 @@
 class Text {
 
 	private static $stemCache = array();
+	
+	/**
+	 * Lists can be started with these marks
+	 */
+	private static $listMarks = array(
+		'-', '*', '+', '°'
+	);
 
+	/**
+	 * converting lists to html
+	 */
+	private static function replaceLists($text) {
+		$text = "\n" . $text;
+
+		// replace non indented simple list
+		$text = preg_replace("@\n-([^- ])@", "\n- $1", $text);
+
+		$splitters = array();
+
+		// @todo make this array static
+		foreach(self::$listMarks as $splitter) {
+			$i = strpos($text, "\n" . $splitter);
+
+			if (false !== $i) {
+				$splitters[$splitter] = $i;
+			}
+		}
+
+		arsort($splitters);
+
+		foreach($splitters as $splitter => $pos) {
+			$text = preg_replace_callback(
+				"/\n((\s?(([" . $splitter . "]) (.*?))\n)+)/siu", 
+				function($match) {
+					return '<ul>' . preg_replace("/^\s?(([" . $match[4] . "]) (.*?))\n/m", "<li>\\3</li>", $match[0]) . '</ul>';
+				},
+				$text);
+		}
+
+		$text = str_replace("<ul>\n", '<ul>', $text);
+
+		return $text;
+	}
+	
+	/**
+	 * Replace some special signs
+	 */
+	private static function replaceSigns($text) {
+		// (tm)
+		$text = preg_replace('/([\040\t])?\(tm\)/i', '\1&trade;', $text);
+	
+		// (c)
+		$text = preg_replace('/\((c|с)\)\s+/iu', '&copy;&nbsp;', $text);
+		
+		// ->
+		$text = preg_replace('/(\s|\>|\&nbsp\;)\-\>(\s|\&nbsp\;|\<\/)/', '\1&rarr;\2', $text);
+		
+		// <-
+		$text = preg_replace('/(\s|\>|\&nbsp\;)\<\-(\s|\&nbsp\;)/', '\1&larr;\2', $text);
+		
+		return $text;
+	}
+	
+	/**
+	 * build a correct link
+	 */	
+	private static function buildLink($href, $value) {
+		if (strlen($value) > 50) {
+			$value = substr($value, 0, 47) . '...';
+		}
+		
+		return '<a href="' . $href . '" rel="noindex, nofollow">' . $value . '</a>';
+	}
+	
 	/**
 	 * TODO shitcode overhead, need to be optimized
 	 */
 	public static function HTMLPrepare($text) {
 		// all links
-		// TODO refactor it (look a little below)
 		if (preg_match_all('/<a(.*?)href="(.*?)"(.*?)>(.*?)<\/a>/siu', $text, $matches)) {
 			$urls = $matches[0];
 		
@@ -17,11 +89,7 @@ class Text {
 				$href = $matches[2][$key];
 				$val  = $matches[4][$key];
 
-				if (strlen($val) > 50) {
-					$val = substr($val, 0, 47) . '...';
-				}
-
-				$text = str_replace($url, '<a href="' . $href . '" rel="noindex,nofollow">' . $val . '</a>', $text);
+				$text = str_replace($url, self::buildLink($href, $val), $text);
 			}
 		}
 	
@@ -50,7 +118,7 @@ class Text {
 		), $text);
 
 		// replace double and more spaces
-		$text = preg_replace('/[ ]+/', ' ', $text);
+		$text = preg_replace('/(\040|\t)+/', ' ', $text);
 
 		$text = str_ireplace(
 			array('<br />', '<br>', "\r"),
@@ -63,61 +131,35 @@ class Text {
 		while (false !== strpos($text, "\n\n\n")) {
 			$text = str_replace("\n\n\n", "\n\n", $text);
 		}
-	
+		
+		$text = self::replaceSigns($text);
+		
 		$text = strip_tags($text, '<a>');
 
-		$text = str_replace(array(' . ', ' , '), array('. ', ', '), $text);
+		// fixing the punctuation spaces
+		$text = preg_replace('/(\040|\t|\&nbsp\;)([\,\:\.])(\s+)/', '\2\3', $text);
+		$text = preg_replace('/(\040|\t|\&nbsp\;)?\,([а-яa-z0-9])/iu', ', \2', $text);
 
 		// convert links to html
 		if (preg_match_all('@([^">=])(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@', $text, $matches)) {
 			foreach($matches[2] as $id => $link) {
-				if (strlen($link) > 50) {
-					$link = substr($link, 0, 47) . '...';
-				}
-
 				$text = str_replace($matches[0][$id], $matches[1][$id] . 
-					'<a href="' . $matches[2][$id] . '" rel="noindex,nofollow">' . $link . '</a>',
+					self::buildLink($matches[2][$id], $link),
 					$text);
 			}
 		}
 
 		// strip last slash from all the links
 		$text = preg_replace("@/</a>@", '</a>', $text);
+		
+		$text = self::replaceLists($text);		
 
-		// converting lists to html
-		$text = "\n" . $text;
-
-		// replace non indented simple list
-		$text = preg_replace("@\n-([^- ])@", "\n- $1", $text);
-
-		$splitters = array();
-
-		// @todo make this array static
-		foreach(array('-', '*', '+', '°') as $splitter) {
-			$i = strpos($text, "\n" . $splitter);
-
-			if (false !== $i) {
-				$splitters[$splitter] = $i;
-			}
-		}
-
-		arsort($splitters);
-
-		foreach($splitters as $splitter => $pos) {
-			$text = preg_replace_callback(
-				"/\n((\s?(([" . $splitter . "]) (.*?))\n)+)/siu", 
-				function($match) {
-					return '<ul>' . preg_replace("/^\s?(([" . $match[4] . "]) (.*?))\n/m", "<li>\\3</li>", $match[0]) . '</ul>';
-				},
-				$text);
-		}
-
-		$text = str_replace("<ul>\n", '<ul>', $text);
-
+		// stripping the last string break
 		while ("\n" === mb_substr($text, strlen($text) - 1, 1)) {
 			$text = mb_substr($text, 0, strlen($text) - 1);
 		}
 	
+		// stripping the first string break
 		while ("\n" === mb_substr($text, 0, 1)) {
 			$text = mb_substr($text, 1, strlen($text) - 1);
 		}

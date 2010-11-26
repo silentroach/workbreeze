857b13417a1d4490bf8c2f580817035bf74b5566
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-11-27 01:04:52 +0300

    beautifier tests migrated to phpUnit

--- tests/BeautifierTest.php
+++ tests/BeautifierTest.php
@@ -1,9 +1,34 @@
 <?php
 
-class BeautifierText extends PHPUnit_Framework_TestCase {
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-	public function testOne() {
-		$this->assertEquals(2, 3);
+class BeautifierTest extends PHPUnit_Framework_TestCase {
+
+	public static function provider() {
+		$result = array();
+
+		foreach (glob(dirname(__FILE__) . DIRECTORY_SEPARATOR . __CLASS__ . DIRECTORY_SEPARATOR . '*.p') as $filename) {
+			$result[] = array($filename);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @dataProvider provider
+	 */
+	public function testBeautifier($filename) {
+		$outname = $filename . 'o';
+
+		$content = trim(file_get_contents($filename));
+		$out     = trim(file_get_contents($outname));
+
+		$conv    = Beautifier::HTMLPrepare($content);
+
+		// more verbose
+		$out     = str_replace('<br />', "\n", $out);
+
+		$this->assertEquals($conv, $out);
 	}
 
 }
